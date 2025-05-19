ae304b22182421906db60f4ec903ba98a323285a
aruzhanrysbayeva <rysbayeva.aruzhan@gmail.com>
2025-05-19 17:23:43 +0500

    Create Vertex.java

---
+++ Vertex.java
@@ -0,0 +1,43 @@
+package ads4;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices = new HashMap<>();
+
+    public Vertex(V data) {
+        this.data = data;
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vertex)) return false;
+        Vertex<?> vertex = (Vertex<?>) o;
+        return data.equals(vertex.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return data.hashCode();
+    }
+}
+
